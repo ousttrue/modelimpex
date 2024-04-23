d81279ef2b09a58b86ef7cd6fac02c4474ab1db0
ousttrue <ousttrue@gmail.com>
2024-04-24 03:12:23 +0900

    rnastubgen

--- rnastubgen.py
+++ rnastubgen.py
@@ -2,10 +2,11 @@
 generate pyi from rna_info
 """
 
-from typing import List, Any
+import bpy  # type: ignore
+import rna_info  #  type: ignore
+
+from typing import Any, Iterator, List
 import io
-import bpy
-import rna_info
 import logging
 import argparse
 import pathlib
@@ -13,79 +14,115 @@ import pathlib
 LOGGER = logging.getLogger(__name__)
 
 
-def print_struct(name: str, s):
-    print(name, type(s))
-    for k in dir(s):
-        print(k, getattr(s, k))
+class StubGenerator:
+    def __init__(self, output: pathlib.Path) -> None:
+        self.output = output
+        self.dependencies = []
+        structs, funcs, ops, props = rna_info.BuildRNAInfo()  # type: ignore
+        self.structs: dict[tuple[str, str], Any] = structs
+        self.resolved: set[str] = set()
 
-
-def type_str(v: Any):
-    match v.type:
-        case "boolean":
-            return "bool"
-
-        case "string":
-            return "str"
-
-        case "enum":
-            return (
-                "Literal["
-                + ",".join([f"'{name}'" for name, value, desc in v.enum_items])
-                + "]"
-            )
-
-        case "pointer":
-            return "Any"
-            # raise NotImplemented()
-
-        case "collection":
-            return "List[Any]"
-
-        case _:
-            return v.type
-
-
-def create(f: io.TextIOBase, s: Any):
-    f.write(
-        f"""# generated
-            
-from typing import Any, List, Literal
-
-class {s.identifier}:
-    ...
-"""
-    )
-    for i, v in enumerate(s.properties):
-        if i == 0:
-            for d in dir(v):
-                print(d)
-        f.write(f"    {v.identifier}: {type_str(v)}\n")
-
-
-def generate(output: pathlib.Path):
-    bpy = output / "bpy"
-    bpy_init = bpy / "__init__.pyi"
-    bpy_init.parent.mkdir(parents=True, exist_ok=True)
-    with bpy_init.open("w", encoding="utf-8") as f:
-        f.write(
-            """
+    def generate(self):
+        bpy = self.output / "bpy"
+        bpy_init = bpy / "__init__.pyi"
+        bpy_init.parent.mkdir(parents=True, exist_ok=True)
+        with bpy_init.open("w", encoding="utf-8") as f:
+            f.write(
+                """
+__all__ = ['types']
 from . import types
 """
+            )
+
+        mod_map: dict[str, List[str]] = {}
+        for d in self._resolve_dependency("Mesh"):
+            s = self.structs[("", d)]
+            mod = mod_map.get(s.module_name)
+            if not mod:
+                mod = []
+                mod_map[s.module_name] = mod
+            mod.append(d)
+
+        # print(mod_map)
+        for k, v in mod_map.items():
+            self._create_module(k, v)
+
+    def _resolve_dependency(self, name: str) -> Iterator[str]:
+        if name in self.resolved:
+            return
+        self.resolved.add(name)
+
+        s = self.structs.get(("", name))  # type: ignore
+        if s:
+            if s.base:
+                for child in self._resolve_dependency(s.base.identifier):
+                    yield child
+            for p in s.properties:
+                match p.type:
+                    case "pointer":
+                        for child in self._resolve_dependency(p.fixed_type.identifier):
+                            yield child
+                    case "collection":
+                        for child in self._resolve_dependency(p.fixed_type.identifier):
+                            yield child
+                    case _:
+                        pass
+
+            yield name
+
+    def _type_str(self, v: Any):
+        match v.type:
+            case "boolean":
+                return "bool"
+
+            case "string":
+                return "str"
+
+            case "enum":
+                return (
+                    "Literal["
+                    + ",".join([f"'{name}'" for name, _, _ in v.enum_items])
+                    + "]"
+                )
+
+            case "pointer":
+                return v.fixed_type.identifier
+                # raise NotImplemented()
+
+            case "collection":
+                return f"List[{v.fixed_type.identifier}]"
+
+            case _:
+                return v.type
+
+    def _create_struct(self, f: io.TextIOBase, name: str):
+        s = self.structs[("", name)]
+        f.write(
+            f"""
+                
+class {s.identifier}({s.base.identifier if s.base else ""}):
+    ...
+"""
         )
+        for i, v in enumerate(s.properties):
+            if i == 0:
+                for d in dir(v):
+                    print(d)
+            f.write(f"    {v.identifier}: {self._type_str(v)}\n")
 
-    structs, funcs, ops, props = rna_info.BuildRNAInfo()
+    def _create_module(self, k: str, v: List[str]):
+        pyi = self.output / (k.replace(".", "/") + ".pyi")
+        # bpy_types_init = bpy / "types.pyi"
+        pyi.parent.mkdir(parents=True, exist_ok=True)
+        with pyi.open("w", encoding="utf-8") as f:
+            f.write(
+                """
+from typing import List, Literal
 
-    bpy_types_init = bpy / "types.pyi"
-    # bpy_types_init.parent.mkdir(parents=True, exist_ok=True)
-    with bpy_types_init.open("w", encoding="utf-8") as f:
-        create(f, structs[("", "Object")])  # type: ignore
-
-    # for k, v in structs.items():
-    #     if v.module_name == "bpy.types":
-    #         print(f"{k[1]}")
-
-    # for d in dir(structs[("", "Object")]):
-    #     print(d)
+"""
+            )
+            for d in v:
+                self._create_struct(f, d)
 
 
 def main():
@@ -96,7 +133,9 @@ def main():
     )
     parser.add_argument("output", type=pathlib.Path)
     args = parser.parse_args()
-    generate(args.output)
+
+    g = StubGenerator(args.output)
+    g.generate()
 
 
 if __name__ == "__main__":
